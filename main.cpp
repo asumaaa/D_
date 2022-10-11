ca70e5b90c1f886c2fa13213d456cc3f85ec8557
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2022-10-11 09:32:47 +0900

    WinAppをクラス化

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
-#include "windows.h"
+#include "WinApp.h"
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+	//ウィンドウ生成
+	WinApp* win = nullptr;
+	win = WinApp::GetInstance();
+	win->CreateWindow_(L"あ");
+
+	//ゲームループ
+	while (true)
+	{
+		//ウィンドウクラスを登録解除
+		win->deleteWindow();
+	}
 	return 0;
 }
