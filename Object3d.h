eaad74aacbc8bf88e105f517aaa84f8d921fe1a6
LC1A_02_asuma <k021g1466@g.neec.ac.jp>
2022-10-17 23:00:30 +0900

    Vector3,Matrix4クラスを追加

--- Object3d.h
+++ Object3d.h
@@ -3,24 +3,24 @@
 #include "d3d12.h"
 #include "dxgi1_6.h"
 #include "cassert"
-#include "vector"
-#include "string"
-#include "assert.h"
-#include "DirectXTex.h"
-#include "wrl.h"
-#include "math.h"
-#include "DirectXMath.h"
-
-class Object3d
-{
-private:
-	//頂点データ構造体
-	struct Vertex
-	{
-		XMFLOAT3 pos;	//座標
-		XMFLOAT3 normalize;	//法線ベクトル
-		XMFLOAT2 uv;	//uv座標
-		Vertex* parent = nullptr;
-	};
-};
+//#include "vector"
+//#include "string"
+//#include "assert.h"
+//#include "DirectXTex.h"
+//#include "wrl.h"
+//#include "math.h"
+//#include "DirectXMath.h"
+//
+//class Object3d
+//{
+//private:
+//	//頂点データ構造体
+//	struct Vertex
+//	{
+//		XMFLOAT3 pos;	//座標
+//		XMFLOAT3 normalize;	//法線ベクトル
+//		XMFLOAT2 uv;	//uv座標
+//		Vertex* parent = nullptr;
+//	};
+//};
 
